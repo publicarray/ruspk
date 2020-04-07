50a20dccff1fd03d4802c6664011d022a6dd27bb
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-04-07 15:25:03 +1000

    Sync Models with schema

--- src/models.rs
+++ src/models.rs
@@ -1,21 +1,99 @@
 use chrono::NaiveDateTime;
-
 use crate::schema::*;
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[table_name = "architecture"]
+pub struct DbArchitecture {
+    pub id: u64,
+    pub code: String,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbPackage, foreign_key = "package_id")]
+#[belongs_to(DbFirmware, foreign_key = "firmware_id")]
+#[table_name = "build"]
+pub struct DbBuild {
+    pub id: u64,
+    pub package_id: u64,
+    pub firmware_id: u64,
+    pub publisher_user_id: u64,
+    pub checksum: String,
+    pub exec_size: u64,
+    pub path: String,
+    pub md5: String,
+    pub insert_date: Option<NaiveDateTime>,
+    pub active: bool,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbBuild, foreign_key = "build_id")]
+#[belongs_to(DbArchitecture, foreign_key = "architecture_id")]
+#[primary_key(build_id, architecture_id)]
+#[table_name = "build_architecture"]
+pub struct DbBuildArchitecture {
+    pub build_id: u64,
+    pub architecture_id: u64,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbLanguage, foreign_key = "language_id")]
+#[belongs_to(DbVersion, foreign_key = "version_id")]
+#[primary_key(language_id, version_id)]
+#[table_name = "displayname"]
+pub struct DbDisplayName {
+    pub version_id: u64,
+    pub language_id: u64,
+    pub name: String,
+}
 
 #[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
 #[table_name = "language"]
 pub struct DbLanguage {
     pub id: u64,
     pub code: String,
-    // pub code: [char;3],
     pub name: String,
 }
 
-#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
-#[table_name = "architecture"]
-pub struct DbArchitecture {
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbBuild, foreign_key = "build_id")]
+#[belongs_to(DbArchitecture, foreign_key = "architecture_id")]
+#[table_name = "download"]
+pub struct DbDownload {
     pub id: u64,
-    pub name: String,
+    pub build_id: u64,
+    pub architecture_id: u64,
+    pub firmware_build: u64,
+    pub ip_address: String,
+    pub user_agent: Option<String>,
+    pub date: Option<NaiveDateTime>,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[table_name = "firmware"]
+pub struct DbFirmware {
+    pub id: u64,
+    pub version: f32,
+    pub build: u64,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbVersion, foreign_key = "version_id")]
+#[table_name = "icon"]
+pub struct DbIcon {
+    pub id: u64,
+    pub version_id: u64,
+    pub size: u16,
+    pub path: String,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
+#[belongs_to(DbLanguage, foreign_key = "language_id")]
+#[primary_key(language_id, version_id)]
+#[table_name = "description"]
+pub struct DbDescription {
+    pub version_id: u64,
+    pub language_id: u64,
+    pub desc: String,
 }
 
 #[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
@@ -25,6 +103,58 @@ pub struct DbPackage {
     pub author_user_id: Option<u64>,
     pub name: String,
     pub insert_date: Option<NaiveDateTime>,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[primary_key(package_id, user_id)]
+#[table_name = "package_user_maintainer"]
+pub struct DbPackageUserMaintainer {
+    pub package_id: u64,
+    pub user_id: u64,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[table_name = "role"]
+pub struct DbRole {
+    pub id: u64,
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[table_name = "screenshot"]
+pub struct DbScreenshot {
+    pub id: u64,
+    pub package_id: u64,
+    pub path: String,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[table_name = "service"]
+pub struct DbService {
+    pub id: u64,
+    pub code: String,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[table_name = "user"]
+pub struct DbUser {
+    pub id: u64,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub api_key: Option<String>,
+    pub github_access_token: Option<String>,
+    pub active: bool,
+    pub confirmed_at: Option<NaiveDateTime>,
+}
+
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[primary_key(user_id, role_id)]
+#[table_name = "user_role"]
+pub struct DbUserRole {
+    pub user_id: u64,
+    pub role_id: u64,
 }
 
 #[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
@@ -52,67 +182,10 @@ pub struct DbVersion {
     pub insert_date: NaiveDateTime,
 }
 
-#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
-#[belongs_to(DbLanguage, foreign_key = "language_id")]
-#[primary_key(language_id, version_id)]
-#[table_name = "description"]
-pub struct DbDescription {
+#[derive(Serialize, Deserialize, Queryable, Identifiable, Debug)]
+#[primary_key(version_id, package_id)]
+#[table_name = "version_service_dependency"]
+pub struct DbVersionServiceDependency {
     pub version_id: u64,
-    pub language_id: u64,
-    pub desc: String,
-}
-
-#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
-#[belongs_to(DbLanguage, foreign_key = "language_id")]
-#[belongs_to(DbVersion, foreign_key = "version_id")]
-#[primary_key(language_id, version_id)]
-#[table_name = "displayname"]
-pub struct DbDisplayName {
-    pub version_id: u64,
-    pub language_id: u64,
-    pub name: String,
-}
-
-#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
-#[table_name = "firmware"]
-pub struct DbFirmware {
-    pub id: u64,
-    pub version: f32,
-    pub build: u64,
-}
-
-#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
-#[belongs_to(DbVersion, foreign_key = "version_id")]
-#[table_name = "icon"]
-pub struct DbIcon {
-    pub id: u64,
-    pub version_id: u64,
-    pub size: u16,
-    pub path: String,
-}
-
-#[derive(Serialize, Deserialize, Queryable, Associations, Identifiable, Debug)]
-#[belongs_to(DbPackage, foreign_key = "package_id")]
-#[belongs_to(DbFirmware, foreign_key = "firmware_id")]
-#[table_name = "build"]
-pub struct DbBuild {
-    pub id: u64,
     pub package_id: u64,
-    pub firmware_id: u64,
-    pub publisher_user_id: u64,
-    pub checksum: String,
-    pub exec_size: u64,
-    pub path: String,
-    pub md5: String,
-    pub insert_date: Option<NaiveDateTime>,
-    pub active: bool,
-}
-
-#[derive(Serialize, Deserialize, Queryable, Associations, Debug)]
-#[belongs_to(DbBuild, foreign_key = "build_id")]
-#[belongs_to(DbArchitecture, foreign_key = "architecture_id")]
-#[table_name = "build_architecture"]
-pub struct DbBuildArchitecture {
-    pub build_id: u64,
-    pub architecture_id: u64,
 }
