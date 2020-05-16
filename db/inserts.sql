29c5c25317c13ff2b9bb4d65222a66516a8076c4
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-05-17 08:07:51 +1000

    🧼

--- db/inserts.sql
+++ db/inserts.sql
@@ -1,22 +1,3 @@
--- -------------------------------------------------------------
--- TablePlus 3.5.0(309)
---
--- https://tableplus.com/
---
--- Database: ruspk
--- Generation Time: 2020-05-17 06:38:50.4460
--- -------------------------------------------------------------
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 INSERT INTO architecture (id, code)
 VALUES
   ('16', 'x64'),
@@ -175,12 +156,3 @@ INSERT INTO displayname (version_id, language_id, displayname) VALUES
 INSERT INTO icon (id, version_id, size, path) VALUES
 ('1', '3', '72', 'default.png'),
 ('2', '3', '256', 'default.png');
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
