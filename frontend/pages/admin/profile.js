5ef6fd20f8fdf3ea528b6298a3ab3f3c2424b397
Sebastian Schmidt <publicarray@users.noreply.github.com>
2023-01-02 12:09:20 +1100

    cleanup

--- frontend/pages/admin/profile.js
+++ frontend/pages/admin/profile.js
@@ -1,28 +1,11 @@
-// import { withIronSession } from "next-iron-session";
 import Layout from "../../components/layout-admin";
 import Button from "../../components/button";
 import useSWR from 'swr'
 import { fetchJsonWithAuth, API, API_VER } from "../../utils";
 
-function handler(req, res, session) {
-    const user = req.session.get("user");
-    res.send({ user });
-}
-
-//   export default withIronSession(handler, {
-//     password: "complex_password_at_least_32_characters_long",
-//     cookieName: "myapp_cookiename",
-//     // if your localhost is served on http:// then disable the secure flag
-//     cookieOptions: {
-//       secure: process.env.NODE_ENV === "production",
-//     },
-//   });
-
-
 export default function ProfilePage() {
     const url = `${API}/${API_VER}/profile`;
     let { data, error, isLoading } = useSWR(`${url}`, fetchJsonWithAuth);
-    // console.log("Is data ready?", !!data);
     if (error) return console.error(error); <div>failed to load</div>;
     if (isLoading) return <div>loading...</div>;
     if (!data) return <div>loading...</div>;
